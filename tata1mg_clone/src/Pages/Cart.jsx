0a57f03ef06269f663fe004c972fbd720a788345
Deepak1095 <ddhankhar999@gmail.com>
2023-03-29 23:27:32 +0530

    cart

--- tata1mg_clone/src/Pages/Cart.jsx
+++ tata1mg_clone/src/Pages/Cart.jsx
@@ -1,22 +1,24 @@
-import { Image } from '@chakra-ui/react'
-import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
-import { useSelector } from 'react-redux'
 
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
+import { useSelector } from 'react-redux'
+import { Box, Image, Flex } from '@chakra-ui/react'
 export const Cart = () => {
     const [data,setData]=useState([])
     useEffect(()=>{
         axios.get("https://quaint-fawn-dungarees.cyclic.app/lending_Page")
-        .then((res)=>setData(res.data))
+        .then((res)=>setData(res.data.medicine_Offers))
     },[])
+    console.log(data.image)
    // const cart=useSelector(store=>store.Cart)
   return (
-    <Box>
-        {data.map((el)=>{(
-            <Flex>
-            <Box>
-             <Image src={el.image}/>
+    
+    <Flex >
+    <Box border="1px solid red" width="60%">
+        {data?.map((el)=>{
+            return <Flex >
+         <Box>
+             <Image width={"100px"} src={el.image}/>
             </Box>
             <Box>
                 <Box>
@@ -24,14 +26,17 @@ export const Cart = () => {
                 </Box>
             </Box>
             </Flex>
-        )})}
-        <Box>
+           
+})}
+         </Box>
+        <Box ml="30">
             <Box>Total Price</Box>
             <Box>Discount</Box>
             <Box>To be paid</Box>
             <Box>Total Saving</Box>
             <Box>Checkout</Box>
         </Box>
-    </Box>
+    </Flex>
+   
   )
 }
